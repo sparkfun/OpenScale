332c9454bb5f64e89a6d1ae296a0c8c2e40bcca4
Mary West <mary.west@sparkfun.com>
2016-05-24 17:01:08 -0600

    adding header files for each sketch

---
+++ firmware/OpenScale/Openscale_PowerReduction/openscale.h
@@ -0,0 +1,39 @@
+//Arduino pin connections to the DAT and CLK pins on the HX711 IC
+#define DAT  2
+#define CLK  3
+
+#define tmp102Address 0x49 //This is the 7-bit address of the TMP102. On OpenScale it has ADR0 tied to VCC.
+
+//Internal EEPROM locations for user settings
+#define LOCATION_MASS_UNITS                     0x01
+#define LOCATION_REPORT_RATE_MSB                (LOCATION_MASS_UNITS + 1)
+#define LOCATION_REPORT_RATE_LSB                (LOCATION_MASS_UNITS + 2)
+#define LOCATION_CALIBRATION_FACTOR_MSB         (LOCATION_MASS_UNITS + 3)
+#define LOCATION_CALIBRATION_FACTOR_MIDHIGH     (LOCATION_MASS_UNITS + 4)
+#define LOCATION_CALIBRATION_FACTOR_MIDLOW      (LOCATION_MASS_UNITS + 5)
+#define LOCATION_CALIBRATION_FACTOR_LSB         (LOCATION_MASS_UNITS + 6)
+#define LOCATION_BAUD_MSB	                (LOCATION_MASS_UNITS + 7)
+#define LOCATION_BAUD_MIDHIGH	                (LOCATION_MASS_UNITS + 8)
+#define LOCATION_BAUD_MIDLOW	                (LOCATION_MASS_UNITS + 9)
+#define LOCATION_BAUD_LSB        	        (LOCATION_MASS_UNITS + 10)
+#define LOCATION_TARE_POINT_MSB                 (LOCATION_MASS_UNITS + 11)
+#define LOCATION_TARE_POINT_MIDHIGH             (LOCATION_MASS_UNITS + 12)
+#define LOCATION_TARE_POINT_MIDLOW              (LOCATION_MASS_UNITS + 13)
+#define LOCATION_TARE_POINT_LSB                 (LOCATION_MASS_UNITS + 14)
+#define LOCATION_TIMESTAMP_ENABLE               (LOCATION_MASS_UNITS + 15)
+#define LOCATION_DECIMAL_PLACES                 (LOCATION_MASS_UNITS + 16)
+#define LOCATION_AVERAGE_AMOUNT                 (LOCATION_MASS_UNITS + 17)
+#define LOCATION_LOCAL_TEMP_ENABLE              (LOCATION_MASS_UNITS + 18)
+#define LOCATION_REMOTE_TEMP_ENABLE             (LOCATION_MASS_UNITS + 19)
+#define LOCATION_STATUS_ENABLE                  (LOCATION_MASS_UNITS + 20)
+#define LOCATION_SERIAL_TRIGGER_ENABLE          (LOCATION_MASS_UNITS + 21)
+#define LOCATION_RAW_READING_ENABLE             (LOCATION_MASS_UNITS + 22)
+#define LOCATION_TRIGGER_CHARACTER              (LOCATION_MASS_UNITS + 23)
+
+//Arduino doesn't properly handle bauds lower than 500bps
+#define BAUD_MIN  1200
+#define BAUD_MAX  1000000
+
+#define UNITS_KG  0
+#define UNITS_LBS 1
+
